352a9dc5dde9eba475d4353101380e2fbc23e6de
Manikanta <kmanikanta@micron.com>
2019-09-27 18:44:38 +0530

    custom hooks

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PlayNumber from './PlayNumber';
 import StarsDisplay from './StarsDisplay';
 import PlayAgain from './PlayAgain';
+import useGameState from "./GameState";
 import utils from './utils';
 import logo from './logo.svg';
 import './App.css';
@@ -9,32 +10,17 @@ import { statSync } from 'fs';
 
 function Game(props) {
 
-  const [stars, setStars] = useState(utils.random(1, 9));
-  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-  const [candidateNums, setCandidateNums] = useState([]);
-  const [secondsLeft, setSecondsLeft] = useState(10);
-
-  useEffect(() => {
-    if (secondsLeft > 0 && availableNums.length > 0) {
-      const timerId = setTimeout(() => {
-        setSecondsLeft(secondsLeft - 1);
-      }, 1000);
-
-      return () => { clearTimeout(timerId) }
-    }
-  });
+  const { stars, availableNums, candidateNums, secondsLeft, setGameState } = useGameState();
 
   const candidatesAreWrong = utils.sum(candidateNums) > stars;
-  // const gameIsDone = availableNums.length === 0;
-  // const gameIsLost = secondsLeft == 0;
 
   const gameStatus = availableNums.length === 0 ? 'won' : secondsLeft === 0 ? 'lost' : 'active';
 
-  const resetGame = () => {
-    setStars(utils.random(1, 9));
-    setAvailableNums(utils.range(1, 9));
-    setCandidateNums([]);
-  }
+  // const resetGame = () => {
+  //   setStars(utils.random(1, 9));
+  //   setAvailableNums(utils.range(1, 9));
+  //   setCandidateNums([]);
+  // }
 
   //arrow function
   const numberStatus = (number) => {
@@ -58,14 +44,7 @@ function Game(props) {
     //candidateNums
     const newCandidateNums = currentStatus == 'available' ? candidateNums.concat(number) : candidateNums.filter(cn => cn !== number);
 
-    if (utils.sum(newCandidateNums) !== stars) {
-      setCandidateNums(newCandidateNums);
-    } else {
-      const newAvailableNums = availableNums.filter(n => !newCandidateNums.includes(n));
-      setStars(utils.randomSumIn(newAvailableNums, 9));
-      setAvailableNums(newAvailableNums);
-      setCandidateNums([]);
-    }
+    setGameState(newCandidateNums);
   }
 
   return (
